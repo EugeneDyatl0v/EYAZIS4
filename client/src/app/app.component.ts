786a0e3eae8b716cf24ff9979fa1669f05125028
Eugene <evgenii.dyatlov06@gmail.com>
2024-12-19 16:37:43 +0300

    upd: add cors and fix nav sidebar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,7 +4,7 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {RouterModule, RouterOutlet} from "@angular/router";
 import {MatIcon} from "@angular/material/icon";
-import {MatIconButton} from "@angular/material/button";
+import {MatIconAnchor, MatIconButton} from "@angular/material/button";
 import {MatDrawer, MatDrawerContainer} from "@angular/material/sidenav";
 import {ArticleComponent} from "./component/article/article.component";
 import {ArticleTextComponent} from "./component/article-text/article-text.component";
@@ -15,6 +15,8 @@ import {NgForOf, NgIf} from '@angular/common';
 import {MatList, MatListItem, MatListSubheaderCssMatStyler} from '@angular/material/list';
 import {MatLine} from '@angular/material/core';
 import {MatDivider} from '@angular/material/divider';
+import {Router} from "@angular/router";
+import {ResponseData} from './model/ResponseData';
 
 @Component({
   selector: 'app-root',
@@ -36,7 +38,8 @@ import {MatDivider} from '@angular/material/divider';
     MatListItem,
     MatLine,
     MatDivider,
-    NgIf
+    NgIf,
+    MatIconAnchor
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -45,7 +48,8 @@ export class AppComponent  implements OnInit{
   articles!: Article[];
 
   constructor(
-    private service: ArticleServiceService
+    private service: ArticleServiceService,
+    private router: Router,
   ) {
   }
 
@@ -55,6 +59,34 @@ export class AppComponent  implements OnInit{
         this.articles = response.data;
       }
     )
+  }
+
+  openText(id: string) {
+    this.router.navigate([`/articles/${id}/`])
+  }
+
+  translateText(id: string, language: string) {
+    let target_language = '';
+    if (language == 'en') {
+      target_language = 'ru'
+    }
+    else {
+      target_language = 'en'
+    }
+
+    this.service.translateArticle(id, target_language).subscribe(
+      response => {
+        this.service.getAllArticles().subscribe(
+          response => {
+            this.articles = response.data;
+          }
+        )
+      },
+      error => {
+        console.log('pizdec');
+      }
+    );
+
   }
   title = 'client';
 }
--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from server.api.article.views import article_router
 app = FastAPI(
 )
+
+allow_origins = [
+    'https://localhost:4200',
+]
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+    )
+
 
 app.include_router(article_router)
